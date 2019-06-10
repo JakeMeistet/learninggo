f9c7581dddb0d3d803e0954db2513678f0fe550d
Jake Bailey <jake.bailey2801@gmail.com>
2019-06-10 15:21:29 +0100

    10/06/2019 15:21

--- programming fundamentals/struct/001-embedded-struct.go
+++ programming fundamentals/struct/001-embedded-struct.go
@@ -4,24 +4,35 @@ import "fmt"
 
 type person struct {
 	first string
-	last string
+	last  string
+}
+
+type secret_agent struct {
+	person
+	ltk bool
 }
 
 func main() {
-	p1 := person{
-		first: "James",
-		last: "Bond",
+	p1 := secret_agent{
+		person: person{
+			first: "James",
+			last:  "Bond",
+		},
+		ltk: true,
 	}
 
-	p2 := person{
-		first: "Miss",
-		last: "Moneypenny",
+	p2 := secret_agent{
+		person: person{
+			first: "Miss",
+			last:  "Moneypenny",
+		},
+		ltk: false,
 	}
 
 	fmt.Println(p1)
-	fmt.Println(p2, "\n")
-
-	fmt.Println(p1.first, p1.last)
-	fmt.Println(p2.first, p2.last)
+	fmt.Println(p2)
+	fmt.Println("")
+	fmt.Println(p1.first, p1.last, p1.ltk)
+	fmt.Println(p2.first, p2.last, p2.ltk)
 
 }
--- programming fundamentals/struct/001.go
+++ programming fundamentals/struct/001.go
@@ -1,1 +1,27 @@
-package _struct
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+}
+
+func main() {
+	p1 := person{
+		first: "James",
+		last:  "Bond",
+	}
+
+	p2 := person{
+		first: "Miss",
+		last:  "Moneypenny",
+	}
+
+	fmt.Println(p1)
+	fmt.Println(p2, "\n")
+
+	fmt.Println(p1.first, p1.last)
+	fmt.Println(p2.first, p2.last)
+
+}
