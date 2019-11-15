4882b7b24e195f3195bc53d975578aacf81ab67d
Jake Bailey <jake.bailey2801@gmail.com>
2019-11-15 12:30:38 +0000

    10/06/2019 15:21

--- Hands on exercises - 5/003.go
+++ Hands on exercises - 5/003.go
@@ -1,1 +1,43 @@
 package main
+
+import "fmt"
+
+type vehicle struct {
+	doors int
+	color string
+}
+
+type truck struct {
+	vehicle
+	fourWheel bool
+}
+
+type sedan struct {
+	vehicle
+	luxury bool
+}
+
+func main() {
+
+	t := truck{
+		vehicle: vehicle{
+			doors: 2,
+			color: "Coffee",
+		},
+		fourWheel: true,
+	}
+	s := sedan{
+		vehicle: vehicle{
+			doors: 3,
+			color: "Red",
+		},
+		luxury: true,
+	}
+
+	fmt.Println(t)
+	fmt.Println(t.doors)
+
+	fmt.Println(s)
+	fmt.Println(s.doors)
+
+}
--- Hands on exercises - 5/004.go
+++ Hands on exercises - 5/004.go
@@ -1,1 +1,36 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	annonymous := struct {
+		fName     string
+		friends   map[string]int
+		favDrinks []string
+	}{
+		fName: "Jake",
+		friends: map[string]int{
+			"Heidi": 1,
+			"Ben":   2,
+		},
+		favDrinks: []string{
+			"Cream Soda",
+			"Coca Cola",
+		},
+	}
+
+	fmt.Println(annonymous)
+	fmt.Println(annonymous.fName)
+	fmt.Println(annonymous.friends["Heidi"])
+	fmt.Println(annonymous.favDrinks[0], "\n")
+
+	for _, v := range annonymous.favDrinks {
+		fmt.Println(v)
+	}
+	fmt.Println()
+	for k, v := range annonymous.friends {
+		fmt.Println(k, v)
+	}
+
+}
